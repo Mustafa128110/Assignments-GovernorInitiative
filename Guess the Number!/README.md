31aa9b8b7d1f03fe2679a295e5cae0e1233d75b7
Mustafa <mustafa.batti128@gmail.com>
2024-04-23 23:46:35 +0500

    added guess the number game project

---
+++ Guess the Number!/README.md
@@ -0,0 +1,17 @@
+# Guess the Number!
+
+A simple game in typescript in which a random number is generated and the user has to guess the number.
+
+## Play Game
+
+```
+npx guess-the-number-mmb
+```
+
+## Packages Used
+
+[inquirer](https://github.com/SBoudrias/Inquirer.js)
+
+```
+npm i inquirer
+```
---
+++ Guess the Number!/main.js
@@ -0,0 +1,115 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber;
+let score = 100;
+let correct = 0;
+console.log("\n\tWelcome to the Number Guessing Game!\n" +
+    "\nInstructions\n" +
+    "  ~ A random number will be generated\n" +
+    "  ~ On every wrong guess your score will be deducted\n" +
+    "  ~ On every correct guess, you will gain score\n" +
+    "  ~ Game ends when your score falls below 0");
+setTimeout(async () => {
+    // ROUND 1
+    randomNumber = Math.floor(Math.random() * 10);
+    console.log(`\n\n\tROUND 1\n\nA random number has been generated (0-9)`);
+    do {
+        let answer = await inquirer.prompt([
+            {
+                name: "guess",
+                type: "number",
+                message: "Make your guess!  ",
+            }
+        ]);
+        if (randomNumber != answer.guess) {
+            score -= 10;
+            correct = 0;
+            console.log(`\nWrong! Your score is ${score}`);
+            if (score > 0) {
+                if (randomNumber > answer.guess) {
+                    console.log(`Guess Higher!\n`);
+                }
+                else {
+                    console.log(`Guess Lower!\n`);
+                }
+            }
+            else {
+                console.log("\nGame Over!\n");
+            }
+        }
+        else {
+            correct = 1;
+            score += 30;
+            console.log("\nCorrect! Lets proceed to Round 2");
+        }
+    } while (score > 0 && correct == 0);
+    // ROUND 2
+    if (correct == 1) {
+        randomNumber = Math.floor(Math.random() * 100);
+        console.log(`\n\n\tROUND 2\n\nA random number has been generated (0-99)`);
+        do {
+            let answer = await inquirer.prompt([
+                {
+                    name: "guess",
+                    type: "number",
+                    message: "Make your guess!  ",
+                }
+            ]);
+            if (randomNumber != answer.guess) {
+                score -= 10;
+                correct = 0;
+                console.log(`\nWrong! Your score is ${score}`);
+                if (score > 0) {
+                    if (randomNumber > answer.guess) {
+                        console.log(`Guess Higher!\n`);
+                    }
+                    else {
+                        console.log(`Guess Lower!\n`);
+                    }
+                }
+                else {
+                    console.log("\nGame Over!\n");
+                }
+            }
+            else {
+                correct = 1;
+                score += 40;
+                console.log("\nCorrect! Lets proceed to Round 3");
+            }
+        } while (score > 0 && correct == 0);
+    }
+    // ROUND 3    
+    if (correct == 1) {
+        randomNumber = Math.floor(Math.random() * 1000);
+        console.log(`\n\n\tROUND 3\n\nA random number has been generated (0-999)`);
+        do {
+            let answer = await inquirer.prompt([
+                {
+                    name: "guess",
+                    type: "number",
+                    message: "Make your guess!  ",
+                }
+            ]);
+            if (randomNumber != answer.guess) {
+                score -= 10;
+                correct = 0;
+                console.log(`\nWrong! Your score is ${score}`);
+                if (score > 0) {
+                    if (randomNumber > answer.guess) {
+                        console.log(`Guess Higher!\n`);
+                    }
+                    else {
+                        console.log(`Guess Lower!\n`);
+                    }
+                }
+                else {
+                    console.log("\nGame Over!\n");
+                }
+            }
+            else {
+                correct = 1;
+                console.log("\nCorrect! \n\nYou Win!\n");
+            }
+        } while (score > 0 && correct == 0);
+    }
+}, 2000);
